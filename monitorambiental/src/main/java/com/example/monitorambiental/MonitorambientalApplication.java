3b673a212be8d5a42209e4ccf4195b279a28a7f6
William David Suarez Arevalo <williamdavidsuarez@proton.me>
2023-01-28 14:20:08 -0500

    iniciation del proyecto

---
+++ monitorambiental/src/main/java/com/example/monitorambiental/MonitorambientalApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitorambiental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorambientalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorambientalApplication.class, args);
+	}
+
+}
